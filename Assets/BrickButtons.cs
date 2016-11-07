d351d149ecdbe83c6af45b199d8db7f8a2b81533
catiejojo <catiejo.pidel@gmail.com>
2016-11-07 13:13:31 +0100

    camera + buttons + touch count

---
+++ Assets/BrickButtons.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BrickButtons : MonoBehaviour {
+	public Button[] buttons;
+
+	public void ExpandPicker() {
+		gameObject.GetComponent<Animation> ().Play ("expand-picker");
+	}
+		
+	public void Select(Button b) {
+		b.GetComponent<RectTransform> ().SetSiblingIndex (buttons.Length - 1);
+		gameObject.GetComponent<Animation> ().Play ("collapse-picker");
+	}
+
+}
+
---
+++ Assets/TouchController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TouchController : MonoBehaviour {
+	public Text touchText;
+	private int touchCount = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Touch myTouch = Input.GetTouch(0);
+
+//		Touch[] myTouches = Input.touches;
+//		foreach (Touch t in myTouches)
+		for(int i = 0; i < Input.touchCount; i++)
+		{
+			if (Input.GetTouch (i).phase == TouchPhase.Began) {
+				touchCount++;
+				//Do something with the touches
+				touchText.text = touchCount + " touches";
+			}
+		}	
+	}
+}
