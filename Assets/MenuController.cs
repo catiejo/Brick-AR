9fbc1fe44ae3539cc36766e449e49ac371fa9b9b
catiejojo <catiejo.pidel@gmail.com>
2016-11-08 14:32:40 +0100

    cleanup

---
+++ Assets/MenuController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+	public Texture[] brickTextures;
+
+	private int _currentTexture = 0;
+	private bool _isAlreadyOpen = false;
+
+	public Texture GetCurrentTexture () {
+		return brickTextures [_currentTexture];
+	}
+
+	public void MenuClicked() {
+		if (_isAlreadyOpen) {
+			CollapseMenu ();
+		} else {
+			ExpandMenu ();
+		}
+	}
+		
+	public void SelectOption(Button option) {
+		option.GetComponent<RectTransform> ().SetSiblingIndex (brickTextures.Length - 1);
+		// NOTE: needs to match order of items in brickTextures
+		switch (option.name) {
+			case "Beige":
+				_currentTexture = 0;
+				break;
+			case "Yellow":
+				_currentTexture = 1;
+				break;
+			case "Green":
+				_currentTexture = 2;
+				break;
+			case "Purple":
+				_currentTexture = 3;
+				break;
+		}
+		CollapseMenu();
+	}
+
+	private void CollapseMenu() {
+		if (_isAlreadyOpen) {
+			gameObject.GetComponent<Animation> ().Play ("collapse-picker");
+		}
+		_isAlreadyOpen = false;
+	}
+
+	private void ExpandMenu() {
+		if (!_isAlreadyOpen) {
+			gameObject.GetComponent<Animation> ().Play ("expand-picker");
+		}
+		_isAlreadyOpen = true;
+	}
+
+}
+
--- Assets/TouchController.cs
+++ Assets/TouchController.cs
@@ -1,44 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using System.Collections;
 using UnityEngine.UI;
+using System.Collections;
 using Tango;
 
 public class TouchController : MonoBehaviour {
-	public Text touchText;
-	public BrickButtons menu;
+	public Text depthText; // For testing purposes
+	public MenuController menu;
 	public Camera camera;
 	public TangoPointCloud pointCloud;
-	private int touchCount = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		for(int i = 0; i < Input.touchCount; i++)
 		{
 			Touch touch = Input.GetTouch (i);
 			if (touch.phase == TouchPhase.Ended && !EventSystem.current.IsPointerOverGameObject()) {
-				PlacePlane (touch.position);
+				PositionBricks (touch.position);
 			}
 		}
 		// For testing purposes
-		if (Input.GetMouseButtonDown (0) && !EventSystem.current.IsPointerOverGameObject()) PlacePlane (Input.mousePosition);
+		if (Input.GetMouseButtonDown (0) && !EventSystem.current.IsPointerOverGameObject()) PositionBricks (Input.mousePosition);
 	}
 
-	void PlacePlane(Vector2 touchCoordinates) {
-		touchText.text = "average depth is: " + pointCloud.m_overallZ; // For testing purposes
+	void PositionBricks(Vector2 touchCoordinates) {
+		depthText.text = "average depth is: " + pointCloud.m_overallZ; // For testing purposes
 		float x = (float)(touchCoordinates.x / Screen.width);
 		float y = (float)(touchCoordinates.y / Screen.height);
 		float z = pointCloud.m_overallZ;
-		Texture brickTexture = menu.GetCurrentTexture ();
-		GameObject bricks = GameObject.CreatePrimitive (PrimitiveType.Quad);
-		bricks.transform.position = camera.ViewportToWorldPoint(new Vector3(x, y, z));
-		bricks.transform.rotation = Quaternion.LookRotation(camera.transform.forward);
-//		bricks.transform.localScale *= 0.1f;
-		bricks.GetComponent<Renderer> ().material.SetTexture("_MainTex", brickTexture);
+		GameObject brickpic = GameObject.CreatePrimitive (PrimitiveType.Quad);
+		brickpic.transform.position = camera.ViewportToWorldPoint(new Vector3(x, y, z));
+		brickpic.transform.rotation = Quaternion.LookRotation(camera.transform.forward);
+		brickpic.GetComponent<Renderer> ().material.SetTexture("_MainTex", menu.GetCurrentTexture ());
 	}
 }
