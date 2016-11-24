ed403cc45f1ea7b9a9f8c0991bcc7749bba84703
catiejojo <catiejo.pidel@gmail.com>
2016-11-24 15:14:28 +0100

    finds surface from vertices!

--- Assets/TouchController.cs
+++ Assets/TouchController.cs
@@ -4,20 +4,24 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using Tango;
+using KDTree;
 
 public class TouchController : MonoBehaviour {
 	public MenuController brickMenu;
 	public Text debug; //For testing purposes
-	public int neighborThreshold = 10;
+	public int neighborCountThreshold = 25;
+	public float neighborDistanceThreshold = .25f;
 	public float planeDistanceThreshold = 0.02f;
 	public Surface surfaceTemplate;
 	public TangoPointCloud tangoPointCloud;
-	//Helper class for PointOctree
+	//Helper class for kdTree
 	public class Point : MonoBehaviour
 	{
-		public Vector3 position;
-		public Point(Vector3 pointPosition) {
-			position = pointPosition;
+		public double[] doublePosition;
+		public Vector3 vectorPosition;
+		public Point(Vector3 point) {
+			doublePosition = new double[3]{point.x, point.y, point.z};
+			vectorPosition = point;
 		}
 	}
 
@@ -31,7 +35,6 @@ public class TouchController : MonoBehaviour {
 				}
 			}
 		}
-		//Mouse tracking for testing in Unity player
 //		if (Input.GetMouseButtonDown (0)) {
 //			if (!CreateSurface (Input.mousePosition)) {
 //				depthText.text = "Unable to find a surface. Please try again.";
@@ -44,17 +47,18 @@ public class TouchController : MonoBehaviour {
 		Plane plane;
 		if (tangoPointCloud.FindPlane (Camera.main, touch, out planeCenter, out plane)) {
 			var surfaceVertices = FindSurfaceVertices (plane, planeCenter);
-			if (surfaceVertices.Count != 0) {
-				Surface surface = Instantiate (surfaceTemplate) as Surface;
-				surface.Create (surfaceVertices, plane, planeCenter, brickMenu.GetCurrentMaterial ());
+//			if (surfaceVertices.Count != 0) {
+//				Surface surface = Instantiate (surfaceTemplate) as Surface;
+//				surface.Create (surfaceVertices, plane, planeCenter, brickMenu.GetCurrentMaterial ());
 				return true;
-			}
+//			}
 		}
 		return false;
 	}
 
 	private List<Vector3> FindSurfaceVertices(Plane plane, Vector3 planeCenter) {
 		var verticesOnSurface = new List<Vector3> ();
+
 		//Step One: Narrow point cloud to points on the plane
 		var verticesOnPlane = new List<Vector3>();
 		for (int i = 0; i < tangoPointCloud.m_pointsCount; i++) {
@@ -63,31 +67,33 @@ public class TouchController : MonoBehaviour {
 				verticesOnPlane.Add (p);
 			}
 		}
-		debug.text = "There are " + verticesOnPlane.Count + " vertices on the plane";
-		//Step Two: Create an octree of the points on the plane
-		var pointCloudOctree = new PointOctree<Point>(1024, planeCenter, 0.25f);
+
+		//Step Two: Create a kd-tree of the points on the plane
+		var kdTree = new KDTree<Point>(3);
 		foreach (var vertex in verticesOnPlane) {
-			pointCloudOctree.Add(new Point (vertex), vertex);
+			var point = new Point (vertex);
+			kdTree.AddPoint(point.doublePosition, point);
 		}
-		//Step Three: Breadth first search for points on surface.
-		Queue<Point> pointsToCheck = new Queue<Point>();
+
+		//Step Three: Breadth-first-style search for points on surface.
+		var pointsToCheck = new UniqueQueue<Point>();
 		pointsToCheck.Enqueue (new Point(planeCenter));
-		while (pointsToCheck.Count > 0) {
+		while (!pointsToCheck.isEmpty()) {
 			var currentPoint = pointsToCheck.Dequeue ();
-			var neighbors = pointCloudOctree.GetNearby (new Ray(Camera.main.transform.position, currentPoint.position), 0.1f);
-			debug.text = "There are " + neighbors.Length + " neighbors nearby";
-			foreach (var neighbor in neighbors) {
-				if (!pointsToCheck.Contains(neighbor)) {
-					pointsToCheck.Enqueue (neighbor);
+			var neighbors = kdTree.NearestNeighbors(currentPoint.doublePosition, neighborCountThreshold, neighborDistanceThreshold);
+			int neighborCount = 0;
+			while (neighbors.MoveNext()) {
+				if (!pointsToCheck.Visited(neighbors.Current)) {
+					pointsToCheck.Enqueue (neighbors.Current);	
 				}
+				neighborCount++;
 			}
-			if (neighbors.Length > neighborThreshold) {
-				verticesOnSurface.Add (currentPoint.position);
+			if (neighborCount >= neighborCountThreshold) {
+				verticesOnSurface.Add (currentPoint.vectorPosition);
 			}
-			pointCloudOctree.Remove (currentPoint); //Already visited
-//			debug.text = "there are " + pointsToCheck.Count + " points left in the queue";
 		}
-//		debug.text = "Found " + verticesOnSurface.Count + " vertices.";
+
+		debug.text = "There are " + verticesOnPlane.Count + " vertices on the plane and " + verticesOnSurface.Count + " vertices on the surface.";
 		return verticesOnSurface;
 	}
 
