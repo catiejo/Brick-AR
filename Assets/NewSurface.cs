b80187ebc5c76e6e66ebcc544f77c2d8e160cec0
catiejojo <catiejo.pidel@gmail.com>
2016-12-05 16:49:04 +0100

    debugging mvp

---
+++ Assets/NewSurface.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using MIConvexHull;
+using System.Collections;
+using System.Collections.Generic; //Lists
+using Tango;
+
+public class NewSurface : MonoBehaviour {
+	private Plane _plane;
+	private Vector3 _planeCenter;
+	private Material _material;
+	private int[] _triangles;
+	private Vector2[] _uv;
+	private Vector3[] _vertices;
+	//Helper class for MIConvexHull
+	public class MIVertex : IVertex
+	{
+		public int Index;
+
+		public double[] Position { get; set; }
+
+		public Vector3 ToVector3()
+		{
+			var position = Position;
+			if (position == null || position.Length < 3)
+				return Vector3.zero;
+			return new Vector3((float)position[0], (float)position[1], (float)position[2]);
+		}
+	}
+
+	public void Create(List<Vector3> worldVertices, Plane plane, Vector3 planeCenter, Material material) {
+		//Member variables
+		_plane = plane;
+		_planeCenter = planeCenter;
+		_material = material;
+		//Plane coordinate system
+		var xaxis = Quaternion.LookRotation(_plane.normal) * Vector3.right; //Horizontal vector transformed to plane's rotation
+		var yaxis = Vector3.Cross(xaxis, _plane.normal);
+		//Position + Rotation
+		transform.position = _planeCenter;
+		transform.rotation = Quaternion.LookRotation (_plane.normal, yaxis);
+		//Set up mesh
+		_vertices = FindLocalVertices(worldVertices);
+		_triangles = FindTriangles ();
+		_uv = FindUV ();
+		CreateMesh ();
+	}
+
+	public void Recreate(List<Vector3> worldVertices) {
+		_vertices = FindLocalVertices(worldVertices);
+		_triangles = FindTriangles ();
+		_uv = FindUV ();
+		CreateMesh ();
+	}
+
+	private void CreateMesh() {
+		Mesh mesh = new Mesh();
+		mesh.Clear();
+		mesh.MarkDynamic();
+		mesh.vertices = _vertices;
+		mesh.uv = _uv;
+		mesh.triangles = _triangles;
+		GetComponent<MeshFilter>().mesh = mesh;
+		GetComponent<MeshRenderer> ().material = _material;
+	}
+
+	private Vector3[] FindLocalVertices(List<Vector3> worldVertices) {
+		var localVertices = new List<Vector3>();
+		foreach (var worldVertex in worldVertices) {
+			localVertices.Add(transform.InverseTransformPoint(worldVertex));
+		}
+		return localVertices.ToArray ();
+	}
+
+	private int[] FindTriangles() {
+		var triangles = new List<int>();
+		var miVertices = new List<MIVertex>();
+		//Convert vertices to MIVertices
+		for (int i = 0; i < _vertices.Length; ++i)
+		{
+			var miVertex = new MIVertex();
+			var vertex = _vertices[i];
+			miVertex.Index = i;
+			miVertex.Position = new double[3]{vertex.x, vertex.y, vertex.z};
+			miVertices.Add(miVertex);
+		}
+		//Generate convex hull + extract triangles
+		var hull = ConvexHull.Create(miVertices);
+		foreach (var face in hull.Faces)
+		{
+			foreach (var vertex in face.Vertices)
+			{
+				triangles.Add(vertex.Index);
+			}
+		}
+		return triangles.ToArray();
+	}
+
+	private Vector2[] FindUV() {
+		var uv = new List<Vector2>();
+		foreach (var vertex in _vertices) {
+			uv.Add(vertex); //Add method knows to discard z coordinate
+		}
+		return uv.ToArray ();
+	}
+
+}
--- Assets/NewTouchController.cs
+++ Assets/NewTouchController.cs
@@ -7,142 +7,138 @@ using Tango;
 using KDTree;
 
 public class NewTouchController : MonoBehaviour {
-	public MenuController brickMenu;
-	public Surface surfaceTemplate;
+	public Text debug;
+//	public MenuController brickMenu;
+//	public Surface surfaceTemplate;
 	public TangoPointCloud tangoPointCloud;
-	private int neighborCountThreshold = 4;
-	private float neighborDistanceThreshold = 0.000175f;
-	private float planeDistanceThreshold = 0.075f;
-	private Touch startPoint;
-	private Touch endPoint;
+//	private int neighborCountThreshold = 4;
+//	private float neighborDistanceThreshold = 0.000175f;
+//	private float planeDistanceThreshold = 0.075f;
+	private Corner topLeft;
+	private Corner bottomRight;
 	private bool isDragging;
-	//Helper class for kdTree
-	public class Point : MonoBehaviour
+	private GameObject line;
+
+//	//Helper class for kdTree
+//	public class Point : MonoBehaviour
+//	{
+//		public double[] doublePosition;
+//		public Vector3 vectorPosition;
+//		public bool visited = false;
+//		public Point(Vector3 point) {
+//			doublePosition = new double[3]{point.x, point.y, point.z};
+//			vectorPosition = point;
+//		}
+//	}
+
+	public class Corner : MonoBehaviour
 	{
-		public double[] doublePosition;
-		public Vector3 vectorPosition;
-		public bool visited = false;
-		public Point(Vector3 point) {
-			doublePosition = new double[3]{point.x, point.y, point.z};
-			vectorPosition = point;
+		public Vector2 screenPosition;
+		public Vector3 worldPosition;
+		public Corner (Vector2 sPos, Vector3 wPos) {
+			screenPosition = sPos;
+			worldPosition = wPos;
 		}
 	}
-
-	/*DEBUGGING*/
-	public Text debug;
-	public Text nCount;
-	public Text nDistance;
-	public Text pDistance;
-	private Surface surface;
-	private Plane surfacePlane;
-	private Vector3 surfacePlaneCenter;
-	public void updateNeighborDistanceThreshold(float newValue) {
-		neighborDistanceThreshold = newValue;
-		Invoke("updateSurface", 0.5f);
-	}
-	public void updatePlaneDistanceThreshold(float newValue) {
-		planeDistanceThreshold = newValue;
-		Invoke("updateSurface", 0.5f);
-	}
-	public void updateNeighborCountThreshold(float newValue) {
-		neighborCountThreshold = (int) newValue;
-		Invoke("updateSurface", 0.5f);
-	}
-	private void updateSurface() {
-		surface.Recreate(FindSurfaceVertices (surfacePlane, surfacePlaneCenter));
-		updateParamText ();
-	}
-	private void updateParamText() {
-		nCount.text = neighborCountThreshold.ToString();
-		nDistance.text = neighborDistanceThreshold.ToString ();
-		pDistance.text = planeDistanceThreshold.ToString();
-	}
-
-	void Awake() {
-		updateParamText ();
-	}
-
 
 	void Update () {
 		if (Input.touchCount > 0)
 		{	
 			Touch touch = Input.GetTouch (0);
+//			Vector3 closestPoint = tangoPointCloud.m_points[tangoPointCloud.FindClosestPoint(Camera.main, touch.position, 100)];
 			if (touch.phase == TouchPhase.Began) {
-				startPoint = touch;
+				debug.text = "Touch started!";
+//				StartLine (point);
+				topLeft = SetCorner (touch.position);
 			} else if (touch.phase == TouchPhase.Moved) {
-				DrawLine (startPoint, touch);
+//				ExtendLine (point);
 			} else if (touch.phase == TouchPhase.Ended) {
-				endPoint = touch;
+				bottomRight = SetCorner (touch.position);
+//				Destroy (line);
+				CreateSurface ();
 			}
 		}
-//		if (Input.GetMouseButtonDown (0)) {
-//			CreateSurface (Input.mousePosition);
-//		}
 	}
 
-	private void DrawLine(Touch start, Touch current) {
-		//make a line from the start point to the current point
+	private void StartLine(Vector3 start)
+	{
+		line = new GameObject();
+		line.transform.position = start;
+		line.AddComponent<LineRenderer>();
+		LineRenderer lr = line.GetComponent<LineRenderer>();
+		lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+		lr.SetColors(Color.cyan, Color.cyan);
+		lr.SetWidth(0.1f, 0.1f);
+		lr.SetPosition(0, start);
+		lr.SetPosition(1, start);
 	}
 
-	private bool CreateSurface(Vector2 touch) {
-		Vector3 planeCenter;
-		Plane plane;
-		if (tangoPointCloud.FindPlane (Camera.main, touch, out planeCenter, out plane)) {
-			var surfaceVertices = FindSurfaceVertices (plane, planeCenter);
-			if (surfaceVertices.Count != 0) {
-				surfacePlane = plane; //For debugging
-				surfacePlaneCenter = planeCenter; //For debugging
-				surface = Instantiate (surfaceTemplate) as Surface;
-				surface.Create (surfaceVertices, plane, planeCenter, brickMenu.GetCurrentMaterial ());
-				return true;
-			}
-			debug.text = "No vertices found on the surface.";
-			return false;
+	private void ExtendLine(Vector3 end) {
+		LineRenderer lr = line.GetComponent<LineRenderer>();
+		lr.SetPosition(1, end);
+	}
+
+	private Color GetCurrentColor() {
+		return Color.cyan;
+	}
+
+	private Corner SetCorner(Vector2 touch) {
+		Vector3 closestPoint = tangoPointCloud.m_points[tangoPointCloud.FindClosestPoint(Camera.main, touch.position, 100)];
+		return new Corner(touch, closestPoint);
+	}
+
+	private bool CreateSurface() {
+		Vector3 midpoint = Vector3.Lerp (topLeft.worldPosition, bottomRight.worldPosition, 0.5f);
+		Vector3 midPlaneCenter;
+		Plane midPlane;
+		if (tangoPointCloud.FindPlane (Camera.main, midpoint, out midPlaneCenter, out midPlane)) {
+			debug.text = "Found a midpoint!";
+			return true;
 		}
-		debug.text = "No surface found.";
+		debug.text = "Please try again.";
 		return false;
 	}
 
-	private List<Vector3> FindSurfaceVertices(Plane plane, Vector3 planeCenter) {
-		var verticesOnSurface = new List<Vector3> ();
-
-		//Step One: Narrow point cloud to points on the plane
-		var verticesOnPlane = new List<Vector3>();
-		for (int i = 0; i < tangoPointCloud.m_pointsCount; i++) {
-			var p = tangoPointCloud.m_points [i];
-			if (Mathf.Abs (plane.GetDistanceToPoint (p)) <= planeDistanceThreshold) {
-				verticesOnPlane.Add (p);
-			}
-		}
-
-		//Step Two: Create a kd-tree of the points on the plane
-		var kdTree = new KDTree<Point>(3);
-		foreach (var vertex in verticesOnPlane) {
-			var point = new Point (vertex);
-			kdTree.AddPoint(point.doublePosition, point);
-		}
-
-		//Step Three: Breadth-first-style search for points on surface.
-		var pointsToCheck = new Queue<Point>();
-		pointsToCheck.Enqueue (new Point(planeCenter));
-		while (pointsToCheck.Count != 0) {
-			var currentPoint = pointsToCheck.Dequeue ();
-			var neighbors = kdTree.NearestNeighbors(currentPoint.doublePosition, neighborCountThreshold, neighborDistanceThreshold);
-			int neighborCount = 0;
-			while (neighbors.MoveNext()) {
-				if (!neighbors.Current.visited) {
-					pointsToCheck.Enqueue (neighbors.Current);
-					neighbors.Current.visited = true;
-				}
-				neighborCount++;
-			}
-			if (neighborCount >= neighborCountThreshold) {
-				verticesOnSurface.Add (currentPoint.vectorPosition);
-			}
-		}
-
-		debug.text = "There are " + verticesOnPlane.Count + " vertices on the plane and " + verticesOnSurface.Count + " vertices on the surface.";
-		return verticesOnSurface;
-	}
+//	private List<Vector3> FindSurfaceVertices(Plane plane, Vector3 planeCenter) {
+//		var verticesOnSurface = new List<Vector3> ();
+//
+//		//Step One: Narrow point cloud to points on the plane
+//		var verticesOnPlane = new List<Vector3>();
+//		for (int i = 0; i < tangoPointCloud.m_pointsCount; i++) {
+//			var p = tangoPointCloud.m_points [i];
+//			if (Mathf.Abs (plane.GetDistanceToPoint (p)) <= planeDistanceThreshold) {
+//				verticesOnPlane.Add (p);
+//			}
+//		}
+//
+//		//Step Two: Create a kd-tree of the points on the plane
+//		var kdTree = new KDTree<Point>(3);
+//		foreach (var vertex in verticesOnPlane) {
+//			var point = new Point (vertex);
+//			kdTree.AddPoint(point.doublePosition, point);
+//		}
+//
+//		//Step Three: Breadth-first-style search for points on surface.
+//		var pointsToCheck = new Queue<Point>();
+//		pointsToCheck.Enqueue (new Point(planeCenter));
+//		while (pointsToCheck.Count != 0) {
+//			var currentPoint = pointsToCheck.Dequeue ();
+//			var neighbors = kdTree.NearestNeighbors(currentPoint.doublePosition, neighborCountThreshold, neighborDistanceThreshold);
+//			int neighborCount = 0;
+//			while (neighbors.MoveNext()) {
+//				if (!neighbors.Current.visited) {
+//					pointsToCheck.Enqueue (neighbors.Current);
+//					neighbors.Current.visited = true;
+//				}
+//				neighborCount++;
+//			}
+//			if (neighborCount >= neighborCountThreshold) {
+//				verticesOnSurface.Add (currentPoint.vectorPosition);
+//			}
+//		}
+//
+//		debug.text = "There are " + verticesOnPlane.Count + " vertices on the plane and " + verticesOnSurface.Count + " vertices on the surface.";
+//		return verticesOnSurface;
+//	}
 
 }
