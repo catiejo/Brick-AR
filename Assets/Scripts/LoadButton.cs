71c9c3df5364663a057fdd173655efd02a126eb2
catiejojo <catiejo.pidel@gmail.com>
2016-12-07 15:57:24 +0100

    prevent back button from crashing app

--- Assets/Scripts/LoadButton.cs
+++ Assets/Scripts/LoadButton.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 public class LoadButton : MonoBehaviour 
 {
 	public void Load(string name) {
+		StartCoroutine(waitBeforeLoading(name, 0.3f));
+	}
+
+	private IEnumerator waitBeforeLoading(string name, float time) {
+		yield return new WaitForSeconds(time);
 		SceneManager.LoadScene (name);
 	}
 }
