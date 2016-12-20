c5e64d4fc02bfe5bea778765cfaa2b5a8b66faa5
catiejojo <catiejo.pidel@gmail.com>
2016-12-20 09:42:16 +0100

    Depth Panel slides in and out of view

---
+++ Assets/DepthPanel.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DepthPanel : MonoBehaviour {
+	private bool _isOpen = false;
+
+	void Start () {
+		SlideDepthPanel (-transform.localScale.x); //start with it closed
+	}
+	
+	void Update () {
+		if (_isOpen && transform.localScale.x < 1.0f) {
+			SlideDepthPanel (0.2f);
+		} else if (!_isOpen && transform.localScale.x > 0) {
+			SlideDepthPanel (-0.2f);
+		}
+	}
+
+	public void ToggleDepthPanel(bool isOpen) {
+		if (isOpen) {
+			ExpandDepthPanel ();
+		} else {
+			CollapseDepthPanel ();
+		}
+	}
+
+	private void SlideDepthPanel(float width) {
+		transform.localScale = new Vector3(transform.localScale.x + width, 1, 1);
+	}
+
+	private void ExpandDepthPanel() {
+		_isOpen = true;
+	}
+	private void CollapseDepthPanel() {
+		_isOpen = false;
+	}
+}
