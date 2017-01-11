1cd5809d5942bc0bbdcd2e47a67c91889a6c7951
catiejojo <catiejo.pidel@gmail.com>
2017-01-11 11:01:43 +0100

    WIP: Overhauling depth panel controller

--- Assets/Occlusion/DepthPanel.cs
+++ Assets/Occlusion/DepthPanel.cs
@@ -4,10 +4,6 @@ using System.Collections;
 public class DepthPanel : MonoBehaviour {
 	private bool _isOpen = false;
 
-	void Start () {
-//		SlideDepthPanel (0); //start with it closed
-	}
-	
 	void Update () {
 		if (_isOpen && transform.localScale.x < 1.0f) {
 			SlideDepthPanel (0.2f);
